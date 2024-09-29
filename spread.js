cc98a67dd5b693e6305e9808e02b213b4c6b8267
sm-mehedi <hasanmehedi63395@gmail.com>
2024-09-29 15:41:06 +0600

    spread operator

---
+++ spread.js
@@ -0,0 +1,7 @@
+const max = Math.max(15,5,6,7,51,4,5,5,4,2,65);
+console.log(max);
+
+const number = [45,6,4,1,5,46,4,585,4,5,];
+const max1 = Math.max(...number);
+console.log(...number);
+console.log(max1);
