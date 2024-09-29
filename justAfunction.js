16d6cbbb37801d2a05d245679453c0d859815489
sm-mehedi <hasanmehedi63395@gmail.com>
2024-09-29 15:36:51 +0600

    object practice

---
+++ justAfunction.js
@@ -0,0 +1,6 @@
+const meaow = (a,b) =>{
+
+    return console.log(`meaow ${a*b}`);
+}
+
+console.log(meaow(10,5));
---
+++ structure.js
@@ -0,0 +1,11 @@
+const meaow = {
+    name:"Pluto",
+    walk(){
+        return `Walk Walk Walk`
+    }
+
+}
+
+meaow.name = "Cutu";
+console.log(meaow.name);
+console.log(meaow.walk());
